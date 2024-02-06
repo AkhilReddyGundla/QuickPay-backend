02b500ca0b9960c09055a43d08acf5cd82b14388
AkhilReddyGundla <121927997+AkhilReddyGundla@users.noreply.github.com>
2024-02-06 17:11:23 +0530

    middleware.js

--- middleware.js
+++ middleware.js
@@ -10,8 +10,9 @@ const authMiddleware = (req,res,next) =>{
     const token = authHeader.split(' ')[1];
     try{
         const decoded = jwt.verify(token,JWT_SECRET);
-        if(decoded.user_id){
-            req.userId = decoded.user_id
+        console.log(decoded)
+        if(decoded.userId){
+            req.userId = decoded.userId
             next()
         }else{
             return res.status(403).json({"Msg" : "Invalid user"})
